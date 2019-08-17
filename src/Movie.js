a06597b88bf9c2c1b9c94f07ba1d23a8da32ae83
lucas0730 <lucas0730@naver.com>
2019-08-18 06:07:22 +0900

    add css

--- src/Movie.js
+++ src/Movie.js
@@ -4,19 +4,21 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie__data">
-      <img src={poster} alt={title} title={title} />
+    <div className="movie">
       <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie__summary">{summary}</p>
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary}</p>
+        </div>
       </div>
     </div>
   );
