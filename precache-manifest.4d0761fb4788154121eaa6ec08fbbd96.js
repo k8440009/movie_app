503c876bf5a24b065e8243bd5eec5529c312e956
lucas0730 <lucas0730@naver.com>
2019-08-18 06:36:23 +0900

    Updates

---
+++ precache-manifest.4d0761fb4788154121eaa6ec08fbbd96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89076ecf6a99ec956c5d61affde6aff1",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "80590928e9430ce5eebc",
+    "url": "/movie_app/static/css/main.bb5532bb.chunk.css"
+  },
+  {
+    "revision": "e85bf0c7afe3d8605f32",
+    "url": "/movie_app/static/js/2.fcb80721.chunk.js"
+  },
+  {
+    "revision": "80590928e9430ce5eebc",
+    "url": "/movie_app/static/js/main.91c2a07a.chunk.js"
+  },
+  {
+    "revision": "f9c5d4d9cea5f935b75a",
+    "url": "/movie_app/static/js/runtime~main.b05fbdd8.js"
+  }
+]);
